0feeac74852cdf5f7e60969de6de5276ed87be82
Paco Nathan <ceteri@gmail.com>
2014-10-07 19:35:34 -0700

    updated the ports to 8888

--- src/run.sh
+++ src/run.sh
@@ -6,4 +6,4 @@ else
     cd /tmp/notebook && git clone https://github.com/$GIT_REPO
 fi
 
-ipython notebook --no-browser --pylab inline --ip=0.0.0.0 --port=8080 --notebook-dir=/tmp/notebook/${GIT_REPO/*\/}/${REPO_DIR}
+ipython notebook --no-browser --pylab inline --ip=0.0.0.0 --port=8888 --notebook-dir=/tmp/notebook/${GIT_REPO/*\/}/${REPO_DIR}
